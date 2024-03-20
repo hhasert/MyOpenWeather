7b57964ad6ca23845764bfc05e83cae009686863
hhasert <hans.hasert@gmail.com>
2024-03-20 16:01:46 +0100

    Commit master

--- app/src/main/java/com/example/myopenweather/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/example/myopenweather/ui/screens/WeatherScreen.kt
@@ -13,13 +13,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -105,31 +109,15 @@ fun CurrentWeatherScreen(
             WeatherIcon(openWeatherCurrent.weatherCondition[0].icon, modifier = Modifier.size(80.dp, 80.dp))
             WeatherInfo(openWeatherCurrent, modifier = Modifier)
        }
-    }
-    Spacer(modifier = Modifier.height(32.dp))
-    Card (
-        modifier = modifier.fillMaxWidth(),
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-        )
-        {
-            Column(
-                modifier = modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(horizontalArrangement = Arrangement.Center,
-                    modifier = modifier){
-
-                  Text("latitude : " + openWeatherCurrent.coordinates.latitude + "     ")
-                  Text("longitude : " + openWeatherCurrent.coordinates.longitude)
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                WeatherDetails( openWeatherCurrent)
-                Spacer(modifier = Modifier.height(16.dp))
-            }
+        Row(horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 26.dp, end = 26.dp) ) {
+            WindSpeedandDirection(openWeatherCurrent.wind.speed, openWeatherCurrent.wind.direction)
+            Text(textAlign = TextAlign.End,text = epochConvertToTime(openWeatherCurrent.datetime).toString())
         }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
         Spacer(modifier = Modifier.height(32.dp))
         Card (
             modifier = modifier.fillMaxWidth(),
@@ -183,8 +171,6 @@ fun WeatherDetails ( openWeatherCurrent: OpenWeatherCurrent,
     {
        Column (modifier.padding(end=4.dp)){
             Text("Weather",modifier.padding(end=44.dp))
-            Text("Wind Speed",modifier.padding(end=19.dp))
-            Text("Wind Direction",modifier.padding(end=2.dp))
             Text("Date",modifier.padding(end=70.dp))
             Text("Time",modifier.padding(end=68.dp))
         }
@@ -192,13 +178,9 @@ fun WeatherDetails ( openWeatherCurrent: OpenWeatherCurrent,
             Text(":")
             Text(":")
             Text(":")
-            Text(":")
-            Text(":")
         }
         Column(){
             Text(openWeatherCurrent.weatherCondition[0].summary)
-            Text(""+ openWeatherCurrent.wind.speed + "m/s")
-            Text("" +  openWeatherCurrent.wind.direction + " deg")
             Text(text = epochConvertToDate(openWeatherCurrent.datetime).toString())
             Text(text = epochConvertToTime(openWeatherCurrent.datetime).toString())
         }
@@ -211,9 +193,9 @@ fun ForecastItem(index : Int, openWeatherForecast: List<ForecastData>)
     val date : LocalDate = epochConvertToDate(openWeatherForecast[index].datetime)
    Card (
        modifier = Modifier
-           .size(width = 64.dp, height = 180.dp)
+           .size(width = 68.dp, height = 240.dp)
            .fillMaxWidth()
-    ) {
+   ) {
        Spacer(modifier = Modifier.height(8.dp))
        Text(textAlign = TextAlign.Center,style = MaterialTheme.typography.labelSmall,
            text = date.dayOfMonth.toString() + "-" + date.monthValue,
@@ -226,18 +208,30 @@ fun ForecastItem(index : Int, openWeatherForecast: List<ForecastData>)
             text = openWeatherForecast[index].weather.temperature.roundToInt().toString() + " \u2103",
             modifier = Modifier.fillMaxWidth() )
        Spacer(modifier = Modifier.height(6.dp))
-       Text(textAlign = TextAlign.Center, text ="rain", modifier = Modifier.fillMaxWidth())
+       Text(textAlign = TextAlign.Center, style = MaterialTheme.typography.labelSmall, text ="rain", modifier = Modifier.fillMaxWidth())
        Text(textAlign = TextAlign.Center, text = "" + (openWeatherForecast[index].precipitationProbability * 100).roundToInt() + " %",
                modifier = Modifier.fillMaxWidth()
             )
+       Spacer(modifier = Modifier.height(8.dp))
+       Text(textAlign = TextAlign.Center,  style = MaterialTheme.typography.labelSmall,text ="wind", modifier = Modifier.fillMaxWidth())
+       Spacer(modifier = Modifier.height(4.dp))
+       Text(textAlign = TextAlign.Center, text = "" + (openWeatherForecast[index].wind.speed).roundToInt() + " m/s",
+           modifier = Modifier.fillMaxWidth()
+       )
+       Row(
+           Modifier.fillMaxWidth(),
+           horizontalArrangement = Arrangement.Center,
+          )
+       {
+           WindIcon(openWeatherForecast[index].wind.direction.toFloat(), Modifier.size(14.dp))
        }
+    }
 }
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun WeatherForecast (openWeatherForecast: OpenWeatherForecast, modifier: Modifier)
 {
-    LazyRow(
-        modifier = modifier.padding(horizontal = 4.dp)
+    LazyRow( modifier = modifier.padding(horizontal = 4.dp)
             .fillMaxWidth()
     )
     {
@@ -245,5 +239,22 @@ fun WeatherForecast (openWeatherForecast: OpenWeatherForecast, modifier: Modifie
         {
             ForecastItem (it, openWeatherForecast.forecast)
         }
+    }
+}
+@Composable
+fun WindIcon ( direction : Float, modifier : Modifier)
+{
+    Icon(
+        Icons.AutoMirrored.Filled.Send,
+        null,
+        modifier.rotate( direction - 270.0f)
+    )
+ }
+@Composable
+fun WindSpeedandDirection ( speed: Double, direction : Int )
+{
+    Row () {
+        Text(textAlign = TextAlign.Start, text = "Wind: " + speed + " m/s  ")
+        WindIcon(direction.toFloat(), Modifier.size(14.dp))
     }
 }
