ba63e38ee17385b53b91beecc79dcc54ae9c94a4
hhasert <hans.hasert@gmail.com>
2024-03-25 13:49:12 +0100

    Commit master

--- app/src/main/java/com/example/myopenweather/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/example/myopenweather/ui/screens/WeatherScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -60,7 +62,10 @@ fun CurrentWeatherScreen(
     openWeatherForecast: OpenWeatherForecast,
     modifier :Modifier =  Modifier,
 ) {
-    Column {
+    Column (
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    )
+    {
         WeatherInfo(openWeatherCurrent)
         Spacer(modifier = Modifier.height(32.dp))
         WeatherForecast(openWeatherForecast = openWeatherForecast, modifier = modifier)
