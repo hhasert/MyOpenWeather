0eaffb151a6b19e5a27b0247f164d383a79f0ff2
hhasert <hans.hasert@gmail.com>
2024-03-27 13:55:17 +0100

    Starting to add a database to hold the locations

---
+++ app/src/main/java/com/example/myopenweather/ui/screens/LocationAddScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myopenweather.ui.screens
+
+import androidx.compose.runtime.Composable
+
+//TODO Add screen for adding location and interact with the database
+
+@Composable
+fun LocationAddScreen ()
+{}
